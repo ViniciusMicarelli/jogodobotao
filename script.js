a848d3f681de00d8b1ce418aaf9d6c65d22241b7
ViniciusMicarelli <125040038+ViniciusMicarelli@users.noreply.github.com>
2024-03-28 11:53:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ button.addEventListener('click', () => {
 autoClickToggleButton.addEventListener('click', () => {
     if (!autoClickActive) {
         autoClickActive = true;
-        autoClickInterval = setInterval(simulateButtonClick, 10);
+        autoClickInterval = setInterval(simulateButtonClick, 100);
         autoClickToggleButton.textContent = 'Desativar Clique Automático';
         autoClickToggleButton.classList.add('active'); // Adiciona a classe "active"
     } else {
